d1339473739e2f83e537a7d1ee38bcded8eff931
James Strachan <49254739+jcstrachan@users.noreply.github.com>
2024-01-07 17:42:56 +0000

    Added gh build

--- src/components/contact.js
+++ src/components/contact.js
@@ -2,6 +2,9 @@ import './contact.css'
 
 export default function Contact() {
     return (
-        <p>This is contact</p>
+        <div className='contact-container'>
+            <h2>Contact Me</h2>
+            If you wish to get in contact with me then feel free to send an email to <b>contact@james-strachan.com</b> and I will get back to you as soon as I can. You can also reach out to me through any of my socials linked at the bottom of the page.
+        </div>
     )
 }
