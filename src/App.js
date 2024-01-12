1ef62fefbef2391461677a4632694d3bc9263196
James Strachan <49254739+jcstrachan@users.noreply.github.com>
2024-01-12 18:30:04 +0000

    Changed to hash routing, removed layout component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
 import './App.css';
-import { useState } from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from './components/layout';
+import { Routes, Route } from "react-router-dom";
 import Home from './components/home';
 import Work from './components/work';
 import About from './components/about';
 import Contact from './components/contact';
+import Navbar from './components/navbar';
+import Footer from './components/footer';
 
 function App() {
-  const [showAlert, setShowAlert] = useState(true);
 
   return (
     <div className="App">
-      <BrowserRouter>
+      <Navbar/>
+      <div className='main-container'>
         <Routes>
-          <Route path='/' element={<Layout/>}>
-            <Route index element={<Home/>}/>
+            <Route path='/' element={<Home/>}/>
             <Route path='/work' element={<Work/>} />
             <Route path='/about' element={<About/>} />
             <Route path='/contact' element={<Contact/>} />
-          </Route>
         </Routes>
-      </BrowserRouter>
+      </div>
+      <Footer/>
     </div>
   );
 }
